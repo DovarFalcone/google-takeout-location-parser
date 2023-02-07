d7cb59ab4d8d2b164b58ed4b9c6b5bbb23734c1d
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-02-06 20:31:12 -0500

    added date column

--- location_history_parser.py
+++ location_history_parser.py
@@ -12,22 +12,23 @@ def process_file(file_path, data_writer):
                     name = obj['placeVisit']['location']['name']
                 except KeyError:
                     name = obj['placeVisit']['location']['address']
+                try:
+                    date_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+                except ValueError: 
+                    date_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+                date_str = date_obj.strftime("%Y-%m-%d")
+                try:
+                    datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+                except ValueError:
+                    datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+                epoch_time = int(datetime_obj.timestamp())
                 lat = obj['placeVisit']['location']['latitudeE7'] / 10**7
                 lon = obj['placeVisit']['location']['longitudeE7'] / 10**7
                 timestamp = obj['placeVisit']['duration']['startTimestamp']
                 address = obj['placeVisit']['location']['address']
                 placeid = obj['placeVisit']['location']['placeId']
-                
-                # Convert the timestamp to a datetime object
-                try:
-                    datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
-                except ValueError:
-                    datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
-                # Convert the datetime object to Unix epoch time
-                epoch_time = int(datetime_obj.timestamp())
-                
                 # Write the data to the CSV file
-                data_writer.writerow([timestamp, lat, lon, address, placeid, name, epoch_time])
+                data_writer.writerow([epoch_time, timestamp, date_str, lat, lon, address, placeid, name])
 
 def main():
     root_dir = "Takeout/Location History/Semantic Location History"
@@ -35,7 +36,7 @@ def main():
 
     with open(output_file, 'w', newline='') as outfile:
         data_writer = csv.writer(outfile)
-        data_writer.writerow(["timestamp", "latitude", "longitude", "address", "placeid", "name", "epoch_time"])
+        data_writer.writerow(["epoch_time", "timestamp", "date", "latitude", "longitude", "address", "placeid", "name"])
 
         for subdir, dirs, files in os.walk(root_dir):
             for file in files:
