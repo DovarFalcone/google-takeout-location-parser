6e48b17151bbbaae86cbe7ba1f1f2227e470745b
iuri <iuri.olari@elara-gmbh.de>
2025-01-02 21:20:58 +0100

    Implement safe data gathering for nested JSON structures

--- semantic_location_parser.py
+++ semantic_location_parser.py
@@ -11,25 +11,27 @@ def process_file(file_path, data_writer):
         with open(file_path, encoding='utf-8-sig') as file:
             data = json.load(file)
             for obj in data['timelineObjects']:
-                if 'placeVisit' in obj:
-                    try:
-                        name = obj['placeVisit']['location']['name']
-                    except KeyError:
-                        name = obj['placeVisit']['location']['address']
-                    lat = obj['placeVisit']['location']['latitudeE7'] / 10**7
-                    lon = obj['placeVisit']['location']['longitudeE7'] / 10**7
-                    timestamp = obj['placeVisit']['duration']['startTimestamp']
-                    address = obj['placeVisit']['location']['address']
-                    placeid = obj['placeVisit']['location']['placeId']
-                    
-                    # Convert the timestamp to a datetime object
-                    try:
-                        datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
-                    except ValueError:
-                        datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
-                    # Convert the datetime object to Unix epoch time
-                    epoch_time = int(datetime_obj.timestamp())
-                    
+                if 'placeVisit' in obj and 'location' in obj['placeVisit']:
+                    location = obj['placeVisit']['location']
+                    address = location.get('address')
+                    name = location.get('name', address)
+                    lat = location.get('latitudeE7') / 10 ** 7
+                    lon = location.get('longitudeE7') / 10 ** 7
+                    placeid = location.get('placeId')
+
+                    timestamp = None
+                    epoch_time = None
+                    if 'duration' in obj['placeVisit']:
+                        timestamp = obj['placeVisit']['duration'].get('startTimestamp')
+                        # Convert the timestamp to a datetime object
+                        if timestamp is not None:
+                            try:
+                                datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+                            except ValueError:
+                                datetime_obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+                            # Convert the datetime object to Unix epoch time
+                            epoch_time = int(datetime_obj.timestamp())
+
                     # Write the data to the CSV file
                     data_writer.writerow([timestamp, lat, lon, address, placeid, name, epoch_time])
     except json.JSONDecodeError as e:
